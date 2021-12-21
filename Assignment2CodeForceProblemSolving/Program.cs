3ed4f4d0c486c4eb5e42e54a955d6ee0fd50eef4
shifa-Alam <shifa7446@gmail.com>
2021-12-21 20:58:35 +0600

    updated

---
+++ Assignment2CodeForceProblemSolving/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Assignment2CodeForceProblemSolving
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter number");
+            var count = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string inputText = Console.ReadLine();
+                if (inputText?.Length > 10)
+                {
+                    Console.WriteLine($"{inputText[0]}{inputText.Length - 2}{inputText[inputText.Length - 1]}");
+                }
+                else
+                {
+                    Console.WriteLine(inputText);
+                }
+            }
+
+        }
+    }
+}
