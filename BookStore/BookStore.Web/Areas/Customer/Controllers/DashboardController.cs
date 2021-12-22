6ad8e17f5c9918fc1cb57066d3469c00f1f5542a
shifa-Alam <shifa7446@gmail.com>
2021-12-22 22:10:52 +0600

    area creation

---
+++ BookStore/BookStore.Web/Areas/Customer/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Web.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
