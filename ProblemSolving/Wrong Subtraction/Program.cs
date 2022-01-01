a766153fff9b5b7d7b339375f72163aa3fad8148
shifa-Alam <shifa7446@gmail.com>
2022-01-01 17:58:25 +0600

    wrong Subtraction

---
+++ ProblemSolving/Wrong Subtraction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Wrong_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputParam = Console.ReadLine();
+            var formaterParam = inputParam.Split(' ');
+            var numberOfSubtraction = Convert.ToInt32(formaterParam[1]);
+            var number = formaterParam[0];
+            //var result = 0;
+            for (int i = 0; i < numberOfSubtraction; i++)
+            {
+                if (number[number.Length - 1] == '0')
+                {
+                    number = number.Remove(number.Length - 1);
+                }
+                else
+                {
+                    number =  (Convert.ToInt32(number)-1).ToString();
+                }
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
