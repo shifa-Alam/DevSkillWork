fdfec26a05d2b63c191271a71f2818cc521288f7
shifa-Alam <shifa7446@gmail.com>
2022-01-01 15:55:19 +0600

    Tram solve

--- ProblemSolving/Tram/Program.cs
+++ ProblemSolving/Tram/Program.cs
@@ -6,7 +6,21 @@ namespace Tram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int stationNumber = Convert.ToInt32(Console.ReadLine());
+            int p = 0, max = 0;
+
+            for (int i = 0; i < stationNumber; i++)
+            {
+                var inputNumbers = Console.ReadLine();
+                var numbers = inputNumbers.Split(' ');
+                p = Convert.ToInt32(numbers[1]) - Convert.ToInt32(numbers[0]) + p;
+                if (max < p)
+                {
+                    max = p;
+                }
+
+            }
+            Console.WriteLine(max);
         }
     }
 }
