0997f45f10bc9c74c5224629b81aecdedc87736b
shifa-Alam <shifa7446@gmail.com>
2022-01-01 17:35:04 +0600

    Soldier and Banana

---
+++ ProblemSolving/Soldier and Bananas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Soldier_and_Bananas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputStatement = Console.ReadLine();
+            var statement = inputStatement.Split(' ');
+            var bananaPrice = Convert.ToInt32(statement[0]);
+            var hisAmount = Convert.ToInt32(statement[1]);
+            var bananaQuantity = Convert.ToInt32(statement[2]);
+
+            var sum = 0;
+
+            for (int i = bananaQuantity; i > 0; i--)
+            {
+                sum = sum + (bananaPrice * i);
+            }
+        
+            Console.WriteLine(hisAmount > sum? 0:(sum-hisAmount));
+        }
+    }
+}
