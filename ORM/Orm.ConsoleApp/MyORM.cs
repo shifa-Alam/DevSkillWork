9ad34e70a2dffaf4d967dfdaa7defee496f3ae4a
shifa-Alam <shifa7446@gmail.com>
2022-02-03 00:23:35 +0600

    updated

--- ORM/Orm.ConsoleApp/MyORM.cs
+++ ORM/Orm.ConsoleApp/MyORM.cs
@@ -2,6 +2,7 @@
 using Orm.ConsoleApp.AdoDotNetCode;
 using Orm.ConsoleApp.Entities;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -44,60 +45,41 @@ namespace Orm.ConsoleApp
         public List<T> GetAll()
         {
             var type = typeof(T);
+
+
             var tableName = type.Name;
             var props = type.GetProperties();
-
-            //IList<T> result = new List<T>();
-            //using (SqlConnection con = new SqlConnection(_connectionString))
-            //{
-            //    SqlCommand cmd = new SqlCommand($"Select * from {typeof(T)};", con);
-            //    con.Open();
-            //    SqlDataReader dataReader = cmd.ExecuteReader();
-
-
-            //    T room = (T)Activator.CreateInstance(typeof(T));
-
-            //    if (dataReader.HasRows)
-            //    {
-            //        while (dataReader.Read())
-            //        {
-
-
-            //            room.pr[0] = Convert.ToInt32(dataReader[0]);
-            //            room.Rent = Convert.ToDouble(dataReader[1]);
-            //            room.HouseId = Convert.ToInt32(dataReader[2]);
-
-            //            result.Add(room);
-            //        }
-            //    }
-
-
-            //}
-            //return result;
 
             Dictionary<string, object> data = new Dictionary<string, object>();
 
             var command = @$"SELECT * FROM {tableName};";
+
             var dataUtility = new DataUtility(_connectionString);
             var Dbresult = dataUtility.GetData(command, data);
 
             List<T> result = new List<T>();
             foreach (var item in Dbresult)
             {
-                //result.Add(item.k)
-                var instance = (T)Activator.CreateInstance(typeof(T));
+                bool isArray = typeof(IEnumerable).IsAssignableFrom(type?.GetType()) ? true : false;
+                var instance = Activator.CreateInstance(typeof(T)) as T;
+
                 foreach (var a in item)
                 {
+
                     var prop = props.FirstOrDefault(p => p.Name.ToLower() == a.Key.ToLower());
                     if (prop != null)
                     {
                         prop.SetValue(instance, a.Value, null);
                     }
 
-                    
+
                 }
+
+
+
                 result.Add(instance);
             }
+
             return result;
 
         }
