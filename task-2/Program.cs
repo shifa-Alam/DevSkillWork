4f1495bdbd635ced4531ec14e5ef2c99d1e56b64
shifa-Alam <shifa7446@gmail.com>
2022-03-19 21:32:06 +0600

    Task 2 Done

---
+++ task-2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputString = Console.ReadLine();
+            var rotationCount =Convert.ToInt64(Console.ReadLine());
+            inputString = inputString.Trim();
+            for (int i = 0; i < rotationCount; i++)
+            {
+                var lastItem =inputString[inputString.Length-1];
+
+                Console.WriteLine(lastItem);
+                inputString= inputString.Remove(inputString.Length - 1, 1);
+                inputString = lastItem+inputString;
+               
+
+
+            }
+            Console.WriteLine(inputString);
+
+        }
+
+
+    }
+}
+
