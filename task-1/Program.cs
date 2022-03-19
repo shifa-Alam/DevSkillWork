558990c2e6af82c955e58698daaae7efeb5da0ef
shifa-Alam <shifa7446@gmail.com>
2022-03-19 21:39:57 +0600

    tt

---
+++ task-1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task1
+{
+    public static class SmartSort
+    {
+        public static IList<T> Sort<T>(this IList<T> elements, Func<T, T, int> compare)
+        {
+            throw new NotImplementedException();
+        }
+       //<T, T, int> delegate  compare();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+        }
+
+
+    }
+}
+
